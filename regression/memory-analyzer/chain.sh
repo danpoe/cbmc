6906897117f0f6e07155dd2c4bd4c6c93c3f2698
Petr Bauch <petr.bauch@gmail.com>
2019-05-09 11:01:10 +0100

    Address comments wrt regression tests
    Mostly specifying why they are not supported now.

--- regression/memory-analyzer/chain.sh
+++ regression/memory-analyzer/chain.sh
@@ -5,8 +5,8 @@ set -e
 memory_analyzer=$1
 goto_gcc=$2
 name=${*:$#}
-name=${name%.exe}
+name=${name%.gb}
 args=${*:3:$#-3}
 
-$goto_gcc -g -std=c11 -o "${name}.exe" "${name}.c"
-$memory_analyzer $args "${name}.exe"
+$goto_gcc -g -std=c11 -o "${name}.gb" "${name}.c"
+$memory_analyzer $args "${name}.gb"
